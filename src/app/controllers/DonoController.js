e6135b1a137bb67979affd17ecb510ca70992b31
larap <laraoliveirati@gmail.com>
2021-07-23 17:00:48 -0300

    Atualização Controllers

--- src/app/controllers/DonoController.js
+++ src/app/controllers/DonoController.js
@@ -1,12 +1,63 @@
-let dono = [
-    {id: 1, nome: "Pedro", contato: "(88) 9 9999-8888", animais_adotados: ""},
+let donos = [
+    {id: 1, cpf: 10020507890, nome: "Pedro Andrade", contato: "(88) 9 9932-8160" },
+    {id: 2, cpf: 94356129050, nome: "Marília Souza", contato: "(88) 9 9708-6345" },
+    {id: 3, cpf: 14519579390, nome: "Robson Ferreira", contato: "(88) 9 9239-1838" },
+    {id: 4, cpf: 10020507580, nome: "Thais Medeiros", contato: "(88) 9 9954-3390" }
 ]
 
 class DonoController{
     //Lista os donos
     index(req,res) {
-        return res.json(dono)
+        return res.json(donos)
     }
+
+    //Recupera um cadastro
+    show(req,res) {
+        const cpf = parseInt(req.params.cpf)
+        const dono = donos.find(items => items.cpf === cpf)
+        const status = dono ? 200 : 404
+
+        return res.status(status).json(dono)
+    }
+
+    //Adiciona um novo cadastro
+    create(req,res) {
+        const {cpf, nome, contato} = req.body
+        const id = donos[donos.length - 1].id + 1
+        
+        const novoDono = {id, cpf, nome, contato}
+        donos.push(novoDono)
+        
+        return res.status(201).json(novoDono)
+    }
+
+    //Atualiza o cadastro
+    update(req,res) {
+        const id = parseInt(req.params.id)
+        const {cpf, nome, contato} = req.body
+
+        const index = donos.findIndex(item => item.id === id)
+        const status = index >= 0 ? 200:404
+
+        if(index >= 0){
+            donos[index] = {id: parseInt(id), cpf: parseInt(cpf), nome, contato}
+        }
+        return res.status(status).json(donos[index])
+    }
+
+    //Exclui um cadastro
+    destroy(req,res) {
+        const cpf = parseInt(req.params.cpf)
+        const index = donos.findIndex(item => item.cpf === cpf)
+        const status = index >= 0 ? 200 : 404
+        
+        if(index >= 0){
+            donos.splice(index, 1)
+        }
+        
+        return res.status(status).json()
+    }
+
 }
 
 export default new DonoController()
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,18 @@ import { Router } from "express"
 const routes = new Router()
 
 import animais from './app/controllers/AnimalController'
-//import dono from './app/controllers/DonoController'
+import donos from './app/controllers/DonoController'
 
 routes.get('/animais', animais.index)
 routes.get('/animais/:id', animais.show)
 routes.post('/animais', animais.create)
 routes.put('/animais/:id', animais.update)
 routes.delete('/animais/:id', animais.destroy)
+
+routes.get('/donos', donos.index)
+routes.get('/donos/:id', donos.show)
+routes.post('/donos', donos.create)
+routes.put('/donos/:id', donos.update)
+routes.delete('/donos/:id', donos.destroy)
 
 export default routes
